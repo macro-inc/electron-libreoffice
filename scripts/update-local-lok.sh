4993cbeffa7aebcbb380d1e83f92649d339703f9
William Hutchinson <willhutchinson2.0@gmail.com>
2022-12-14 09:11:38 -0500

    feat: update-local-lok improvements

--- scripts/update-local-lok.sh
+++ scripts/update-local-lok.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
-
-echo "Ensure you run this from the root of the repo"
+echo "Ensure this is run from the root of the repo"
 
 LOK_PATH=$1
 
+BASE_PATH=$(pwd)
+ELECTRON_OUT_DIR=Default
+DEFAULT_LOK_PATH=../libreofficekit
+
 if [ -z "$LOK_PATH" ]
 then
-  echo "LOK_PATH not set. Defaulting to ../libreofficekit"
-
-  LOK_PATH="../libreofficekit"
+  echo "LOK_PATH not set. Defaulting to $DEFAULT_LOK_PATH"
+  LOK_PATH=$DEFAULT_LOK_PATH
+else
+  echo "LOK_PATH was set to $LOK_PATH"
 fi
+
+OUT_DIR="$BASE_PATH/src/out/$ELECTRON_OUT_DIR"
 
 echo "Removing existing libreofficekit"
 
-rm -rf ./src/out/Default/libreofficekit
-mkdir ./src/out/Default/libreofficekit
+rm -rf $OUT_DIR/libreofficekit
+mkdir $OUT_DIR/libreofficekit
 
 echo "Bringing over local libreofficekit"
 
-cp -rf $LOK_PATH/libreoffice-core/instdir/* ./src/out/Default/libreofficekit/
+cp -rf $LOK_PATH/libreoffice-core/instdir/* $OUT_DIR/libreofficekit/
 
 echo "Complete"
