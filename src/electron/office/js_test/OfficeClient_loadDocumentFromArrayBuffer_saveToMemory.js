a73b55f7d45ff77c226bda29738fee02f469eeb5
Chase Colman <chase@macro.com>
2024-01-12 12:10:43 +0800

    Add failure case for saveToMemory

--- src/electron/office/js_test/OfficeClient_loadDocumentFromArrayBuffer_saveToMemory.js
+++ src/electron/office/js_test/OfficeClient_loadDocumentFromArrayBuffer_saveToMemory.js
@@ -3,6 +3,11 @@ async function testLoadDocumentFromArrayBuffer() {
   const inMemory = await docClient.saveToMemory();
   assert(inMemory.byteLength !== 0);
   assert(await libreoffice.loadDocumentFromArrayBuffer(inMemory) != null);
+  const inMemoryPDF = await docClient.saveToMemory("writer_pdf_Export");
+  assert(inMemoryPDF.byteLength !== 0);
+
+ const invalid = await docClient.saveToMemory("not-a-real-format");
+ assert(invalid == null);
 }
 
 testLoadDocumentFromArrayBuffer();
